27b5f16aefd6393a90bde94d0388c9eb4b40210c
Vraj Patel <vrajbpatel1719@gmail.com>
2025-02-01 12:56:15 +0530

    Method to convert an array into an ArrayList

--- Main.java
+++ Main.java
@@ -28,3 +28,25 @@ class ArrayFunctions {
         System.out.println("Even numbers:");
         display(even_list);
     }
+
+    // Method to find the index of two neighboring numbers with the smallest difference
+    public int minConsecutiveDifference(int[] array) {
+        int index = 0; // Initialize index of the smallest difference
+
+        // Loop through the array to find the smallest consecutive difference
+        for (int i = 0; i < array.length - 1; i++) {
+            if (Math.abs(array[i] - array[i + 1]) < Math.abs(array[index] - array[index + 1])) {
+                index = i; // Update index if a smaller difference is found
+            }
+        }
+        return index; // Return the index of the first number
+    }
+
+	// Method to convert an array into an ArrayList
+    public ArrayList<Integer> convertArrayToArrayList(int[] array) {
+        ArrayList<Integer> array_list = new ArrayList<>();
+        for (int num : array) {
+            array_list.add(num); // Add each element from the array to the ArrayList
+        }
+        return array_list;
+    }
