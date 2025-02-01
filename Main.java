debfe7c433c68f13a88ebbf414cbbef08d50be42
Vraj Patel <vrajbpatel1719@gmail.com>
2025-02-01 12:48:58 +0530

    Method to display Arraylist

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+// Class to handle various array-related operations
+class ArrayFunctions {
+
+    // Method to display an ArrayList
+    public void display(ArrayList<Integer> array_list) {
+        System.out.println("Array is: " + array_list);
+    }
