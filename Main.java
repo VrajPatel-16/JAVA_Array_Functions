e2c516759678afd250ef79b2807cfb4902f83dcc
Vraj Patel <vrajbpatel1719@gmail.com>
2025-02-01 13:04:14 +0530

    Create objects for input and operations

---
+++ Main.java
@@ -0,0 +1,6 @@
+// Main class to execute the program
+class Main {
+    public static void main(String args[]) {
+        // Create objects for input and operations
+        UserInput user_input = new UserInput();
+        ArrayFunctions functions = new ArrayFunctions();
